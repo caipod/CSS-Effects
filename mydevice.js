65aaf1b9f255a8177a2ebfa15b6133d7ff741b63
Cai Fang·蔡放 <cai89757@gmail.com>
2022-05-09 12:18:43 +0800

    新增载入时间显示

--- mydevice.js
+++ mydevice.js
@@ -5,3 +5,4 @@ document.getElementById("colorDepth").innerHTML = +screen.colorDepth + "bit";
 document.getElementById("aspect").innerHTML = aspect_ratio_string;
 document.getElementById("windowpoint").innerHTML = window.innerWidth + '*' + window.innerHeight;
 document.getElementById("userAgent").innerHTML = navigator.userAgent;
+document.getElementById("maindate").innerHTML = Date();
