cf65051c9efb637f305afbbdac630ffc50b87b5f
Cai Fang·蔡放 <cai89757@gmail.com>
2022-10-04 07:00:58 +0800

    修改文案

--- layers/mydevice.js
+++ layers/mydevice.js
@@ -1,6 +1,6 @@
 var resratio = (screen.width/screen.height);
 if (resratio >= 1.6) {var widescreen_string = "Widescreen";}
-else {var widescreen_string = "Not A Widescreen";}
+else {var widescreen_string = "NAWS";}
 if (1.32 < resratio && resratio < 1.34) {var aspect_ratio_string = "4:3";}
 else if (1.77 < resratio && resratio < 1.79) {var aspect_ratio_string = "16:9";}
 else if (1.59 < resratio && resratio < 1.61) {var aspect_ratio_string = "16:10";}
